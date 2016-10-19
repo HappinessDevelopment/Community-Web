93b6fd79d8108e4bb23e34560b6d8f0bbe452bc8
b-porter <beneporter@gmail.com>
2016-10-19 12:50:54 -0700

    add comments to bootstrap scripts

--- bootstrap_venv.py
+++ bootstrap_venv.py
@@ -1,3 +1,4 @@
+# this script executes the virtualenv bootstrapping script.  Destination dir is projectroot/.venv
 import os
 
 os.system(os.path.join(os.getcwd(), "util/venv/bootstrap.py") + " .venv")
--- util/venv/create_virtualenv_bootstrap.py
+++ util/venv/create_virtualenv_bootstrap.py
@@ -1,8 +1,10 @@
+# this script generates the virtualenv bootstrap.py for creating and bootstrapping a virtual env
 import virtualenv
 import os
 s = virtualenv.create_bootstrap_script('''
 import subprocess
 def after_install(options, home_dir=os.path.join(os.pardir, 'venv')):
+  # packages to bootstrap
   subprocess.call(['pip', 'install', 'django'])
   subprocess.call(['pip', 'install', 'djangorestframework'])
 ''')
