d632969b672d6458b5e0a82c045272b7d4be329a
b-porter <beneporter@gmail.com>
2016-10-18 23:34:39 -0700

    update bootstrapping script

---
+++ bootstrap_venv.py
@@ -0,0 +1,3 @@
+import os
+
+os.system(os.path.join(os.getcwd(), "util/venv/bootstrap.py") + " .venv")
--- util/venv/create_virtualenv_bootstrap.py
+++ util/venv/create_virtualenv_bootstrap.py
@@ -1,7 +1,8 @@
 import virtualenv
+import os
 s = virtualenv.create_bootstrap_script('''
 import subprocess
-def after_install(options, home_dir):
+def after_install(options, home_dir=os.path.join(os.pardir, 'venv')):
   subprocess.call(['pip', 'install', 'django'])
   subprocess.call(['pip', 'install', 'djangorestframework'])
 ''')
