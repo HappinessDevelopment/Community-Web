f220fa141f80275fc31ab03c874a8f32f50d4cf0
Benjamin P <beneporter@gmail.com>
2016-10-20 17:49:01 -0700

    update bootstrap_venv.py

--- bootstrap_venv.py
+++ bootstrap_venv.py
@@ -3,20 +3,25 @@
 import os
 import platform
 import subprocess
+import sys
 
+req_version = (3,4)
+cur_version = sys.version_info
+if cur_version < req_version:
+	print('python >= 3.4 required')
+	exit()
 venv_name = '.venv'
 pip_ver = 'pip'
+if os.system('python -m venv ' + venv_name) != 0:
+	os.system('python3.4 -m venv ' + venv_name)
+	pip_ver = 'pip3.4'
 if platform.system() == 'Windows':
-    os.system('python -m venv ' + venv_name)
     os.system(venv_name + '\\Scripts\\activate.bat')
     bin = 'Scripts'
 else:
-    if os.system('pyvenv ' + venv_name) != 0:
-        os.system('python3.4 -m venv ' + venv_name)
-        pip_ver = 'pip3.4'
     bin = 'bin'
 
 print('installing packages')
 
-pip = os.path.join(os.getcwd(), venv_name, bin, 'pip')
+pip = os.path.join(os.getcwd(), venv_name, bin, pip_ver)
 subprocess.call([pip, 'install', '-r', 'requirements.txt'])
