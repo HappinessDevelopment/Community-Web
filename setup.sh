162109289be10d4964317208fbb44ad24218207d
acroutworst <croutworsta@gmail.com>
2016-10-25 18:44:22 -0700

    updated user interface of script

--- setup.sh
+++ setup.sh
@@ -2,19 +2,23 @@
 
 alias python='python3'
 
+echo "Pip installing virtual env..."
 pip install virtualenv
 # create clean virtual environment
 python -m venv .venv
 
 # activate the environment
+echo "Created a virtual env and now activating."
 source .venv/bin/activate
 
 # install pip
+echo "Installing pip for venv..."
 easy_install pip
 
 # install from requirements.txt (currently only django and djangorestframework)
-echo "installing requirements..."
+echo "Installing requirements..."
 pip install -r requirements.txt
 
+echo "Deactivated"
 deactivate 
 
