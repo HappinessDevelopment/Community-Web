8fc2e6e7d703765ba609ceb507556dca0a368255
Benjamin P <beneporter@gmail.com>
2016-10-25 19:23:33 -0700

    stormpath user authentication.  began implementation of this module in the webapp

--- community/webapp/apps.py
+++ community/webapp/apps.py
@@ -1,5 +1,12 @@
 from django.apps import AppConfig
+from os.path import expanduser, join
 
+from stormpath.client import Client as StormpathClient
+
+STORMPATH_API_KEY = join('.stormpath', 'apiKey.properties')
+stormpathClient = StormpathClient(api_key_file=STORMPATH_API_KEY)
+
+stormpathClient.application.search({'name': 'webapp'})[0]
 
 class WebappConfig(AppConfig):
     name = 'webapp'
