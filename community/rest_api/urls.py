6c40e9f3ac2bac285588f6eba82f78a31af7b2b7
b-porter <beneporter@gmail.com>
2016-10-29 23:24:59 -0700

    update rest_api/urls.py

--- community/rest_api/urls.py
+++ community/rest_api/urls.py
@@ -10,5 +10,5 @@ router.register(r'groups', views.group.GroupViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-authentication/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
